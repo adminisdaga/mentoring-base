7f569936af8bb2253ee3eab2ae20eac4ce509548
adminisdaga <dmagomedov978@gmail.com>
2024-12-23 22:00:47 +0300

    ‘dalkmd’

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None 
 })
 export class AppComponent {
   title = 'mentoring';
